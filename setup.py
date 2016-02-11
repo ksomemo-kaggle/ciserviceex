e97d3cbb52bc0724a4a27af9111593b48367b9f5
ksomemo <kntsmy@hotmail.co.jp>
2016-02-11 21:20:22 +0900

    Add file for package version

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import re
 from setuptools import (
     setup,
     find_packages
@@ -5,6 +6,18 @@ from setuptools import (
 
 PACKAGES_ROOT = '.'
 
+def find_version():
+    with open('ciserviceex/version.py') as f:
+        version_regex = r"^__version__ = ['\"]([^'\"]*)['\"]"
+        version_match = re.search(version_regex, f.read(), re.M)
+        if version_match:
+            version = version_match.group(1)
+        else:
+            raise RuntimeError("Unable to find version string.")
+    return version
+
 setup(
+    version=find_version(),
     packages=find_packages(PACKAGES_ROOT)
 )
+
